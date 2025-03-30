01ce70254991a3a4add120e871dc7adb3fdd2208
epheX01 <stefanvallendar@gmail.com>
2025-03-30 13:24:02 +0200

    main and stats

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from stats import count_book_words
-
+from stats import *
 def main():
+    count = character_count(get_book_text(file_path))
     print(f"{count_book_words()} words found in the document")
-
+    print(f"{count}")
 main()
--- stats.py
+++ stats.py
@@ -10,6 +10,14 @@ def count_book_words():
     wordcount = text.split()
     return len(wordcount)
 
-def character_count():
-    text = get_book_text(file_path)
-    
+def character_count(text):
+    text = text.lower()
+    # set_compare = set(text)
+    char_count = {}
+    for char in text:
+        if char in char_count:
+            char_count[char] += 1
+        else: 
+            char_count[char] = 1
+
+    return char_count
